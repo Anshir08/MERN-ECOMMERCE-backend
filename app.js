efd7a9425ed3d199af6a18d3a531957f680e90f1
Anshir08 <chaudharyanshir@gmail.com>
2024-03-10 16:19:41 +0530

    cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
+const cors = require("cors");
 
 const errorMiddleware = require("./middleware/error");
 
@@ -15,6 +16,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(fileUpload());
+app.use(
+  cors({
+  origin: [process.env.FRONTEND_URL],
+  methods:["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+})
+);
 
 // Route Imports
 const product = require("./routes/productRoute");
@@ -32,11 +40,11 @@ app.use("/api/v1", payment);
 //   })
 // });
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
 
 app.use(errorMiddleware);
 
