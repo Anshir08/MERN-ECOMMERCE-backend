c1dd8e2abc0e6e43d4f917cf52d4ab629cfe7e12
Anshir08 <chaudharyanshir@gmail.com>
2024-03-11 08:30:54 +0530

    http

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,8 +57,7 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
 // Logout User
 exports.logout = catchAsyncErrors(async (req, res, next) => {
   res.cookie("token", null, {
-    expires: new Date(Date.now()),
-    httpOnly: true,
+    expires: new Date(Date.now())
   });
 
   res.status(200).json({
