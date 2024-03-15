b7c6b58a0e072875a254ab7fd8d4844d1f144c0e
Anshir08 <chaudharyanshir@gmail.com>
2024-03-15 17:44:20 +0530

    size

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({path: "config/config.env"});
 }
 
-app.use(express.json({limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+app.use(express.json());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(fileUpload());
 app.use(
   cors({
