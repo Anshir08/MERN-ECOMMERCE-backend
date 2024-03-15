ae3cba4a79cfcd3a4ae21730a27e014bcb73ac76
Anshir08 <chaudharyanshir@gmail.com>
2024-03-15 17:07:46 +0530

    file limit

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({path: "config/config.env"});
 }
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.bodyParser({limit: '50mb'}));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({limit: '50mb',extended: true}));
 app.use(fileUpload());
 app.use(
   cors({
